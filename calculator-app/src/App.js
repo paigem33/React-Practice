f83e611aa1604cfa9a9a1e9d2fd5be462ae0fe3f
Ubuntu <ubuntu@ip-172-31-88-90.ec2.internal>
2019-07-10 23:50:50 +0000

    worked on calc app

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -16,21 +16,61 @@ export default class App extends Component {
   
   onClick = (button) => {
     let {result, num1, num2, operation} = this.state
-    if (num1 === ""){
+    let isNum = /\d/
+    //if the first button pushed is not a number and num1 does not have a value
+    if (isNum.test(button) === false && num1 === ''){
+      console.log('not a number')
+      //if button pushed is a number and num1 is empty
+    } else if (isNum.test(button) === true && num1 === ''){
+      // let buttonNew = parseFloat(button)
       this.setState({num1:button})
       console.log(button)
-    } else if (operation === ""){
-      this.setState({operation:button})
-      console.log(button)
-    } else if (num2 === ""){
+      //if the button pushed is a number and there is already a value in num1
+    } else if (isNum.test(button) === true && num1 !== '' && operation === ''){
+      let newValue = num1+button
+      this.setState({num1:newValue})
+      console.log(newValue)
+      //if a non number button is pushed and there is a value in num1 and no value in num2
+    } else if (isNum.test(button) === false && num1 !== '' && num2 === ''){
+      //clear num1 if C is pressed
+      if (button === 'C' || button === 'CE'){
+        this.setState({num1:''})
+        //if button is +
+      } else if (button === '+' || button === '-' || button === 'X' || button === '/'){
+        this.setState({operation:button})
+        console.log(button)
+      }
+      //if buttonpressed is a number and num1 has a value and operation has a value and num2 does not have a value
+    } else if (isNum.test(button) === true && num1 !== '' && operation !== '' && num2 === ''){
       this.setState({num2:button})
       console.log(button)
-    } else if (button === "="){
-      let newNum1 = parseFloat(num1)
-      let newNum2 = parseFloat(num2)
-      result = newNum1+operation+newNum2
-      this.setState({result})
+      //if button pushed is a number and there is already a value for num1 and a value for operation and a value for num2
+    } else if (isNum.test(button) === true && num1 !== '' && operation !== '' && num2 !== ''){
+      let newValue = num2+button
+      this.setState({num2:newValue})
+      console.log(newValue)
+      //if button pressed is C and num2 has a value
+    } else if (num2 !== '' && button === 'C'){
+      this.setState({num2:''})
       console.log(button)
+      //if num2 has a value and button pressed is CE
+    } else if (num2 !== '' && button === 'CE'){
+      this.setState({num1:''})
+      this.setState({num2:''})
+      this.setState({operation:''})
+      console.log(button)
+    } else if (num1 !== '' && num2 !== '' && operation === '+' && button === '='){
+      let outcome = parseFloat(num1)+parseFloat(num2)
+      this.setState({result:outcome})
+      console.log(outcome)
+    } else if (num1 !== '' && num2 !== '' && operation === '-' && button === '='){
+      let outcome = parseFloat(num1)-parseFloat(num2)
+      this.setState({result:outcome})
+      console.log(outcome)
+    } else if (num1 !== '' && num2 !== '' && operation === 'X' && button === '='){
+      let outcome = parseFloat(num1)*parseFloat(num2)
+      this.setState({result:outcome})
+      console.log(outcome)
     }
   }
   
